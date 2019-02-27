54e6e255a57784ed15dd121fe344622f9b2fb971
Andrij Chizhenko <chizh.and.ol@gmail.com>
2019-02-27 13:24:46 +0200

    1 commit

---
+++ src/chizhenko/Main.java
@@ -0,0 +1,14 @@
+package chizhenko;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 
+		Triangle tr1 = new Triangle(2,4.5,5);
+		Triangle tr2 = new Triangle(2,4,5);
+		
+		tr1.getSquare();
+		tr2.getSquare();
+	}
+
+}
